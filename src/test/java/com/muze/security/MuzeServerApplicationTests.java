08c5d008be85579cd08729846ab3c3752335bc2d
yoogle96 <dmltjs851@gmail.com>
2020-01-18 14:04:52 +0900

    Init first init

---
+++ src/test/java/com/muze/security/MuzeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muze.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
