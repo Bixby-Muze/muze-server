67584c5537b2376720f8329ae9aa1c64981fc2c7
yoogle96 <dmltjs851@gmail.com>
2020-01-20 02:36:51 +0900

    Update DailyBoxOfficeController class init

--- src/main/java/com/muze/api/movie/controller/DailyBoxOfficeController.java
+++ src/main/java/com/muze/api/movie/controller/DailyBoxOfficeController.java
@@ -1,4 +1,16 @@
 package com.muze.api.movie.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DailyBoxOfficeController {
+    @RequestMapping(value = "/dailyBoxOfficeTest", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public String test() {
+        return "I`m Aliver!";
+    }
 }
