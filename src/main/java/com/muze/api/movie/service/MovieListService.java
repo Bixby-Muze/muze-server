969510405078c334b92ac503e0818026621e0241
yoogle96 <dmltjs851@gmail.com>
2020-01-20 05:03:14 +0900

    Update MovieListService init

--- src/main/java/com/muze/api/movie/service/MovieListService.java
+++ src/main/java/com/muze/api/movie/service/MovieListService.java
@@ -1,5 +1,34 @@
 package com.muze.api.movie.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+@Service
 public class MovieListService {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public MovieListService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Value("${api.url.apiBaseUrl}")
+    private String baseUrl;
+
+    @Value("${api.url.movieListUrl}")
+    private String movieListUrl;
+
+    @Value("${api.key}")
+    private String key;
+
+    public String getAll() {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl + movieListUrl)
+                .queryParam("key", key);
+
+        return restTemplate.getForObject(builder.toUriString(), String.class);
+    }
 }
