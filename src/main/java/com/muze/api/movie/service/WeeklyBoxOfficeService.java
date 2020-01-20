70d40ec00dcd947bdc34506007f71b67631f0eee
ooeunz <yuns994@gmail.com>
2020-01-20 18:03:35 +0900

    [ooeunz] 영화 api 불러오기

--- src/main/java/com/muze/api/movie/service/WeeklyBoxOfficeService.java
+++ src/main/java/com/muze/api/movie/service/WeeklyBoxOfficeService.java
@@ -1,10 +1,14 @@
 package com.muze.api.movie.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class WeeklyBoxOfficeService {
@@ -29,6 +33,10 @@ public class WeeklyBoxOfficeService {
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(baseUrl + weeklyBoxOfficeUrl)
                 .queryParam("key", key)
                 .queryParam("targetDt", targetDt);
+
+//        Map<String, Object> map = new HashMap<String, Object>();
+//        map = restTemplate.getForObject(builder.toUriString(), Map.class);
+//        System.out.println(map.get("boxOfficeResult"));
 
         return restTemplate.getForObject(builder.toUriString(), String.class);
     }
