7d1cc26a7a6007489b91e557e4e1b201e0315e34
yoogle96 <dmltjs851@gmail.com>
2020-02-02 17:11:20 +0900

    Update modify date string format

--- src/main/java/com/muze/api/movie/common/MuzeUtil.java
+++ src/main/java/com/muze/api/movie/common/MuzeUtil.java
@@ -8,14 +8,11 @@ public class MuzeUtil {
     public String convertDateStringFormat(String date) {
         try {
             StringBuffer stringBuffer = new StringBuffer(date);
-            System.out.println("yoogle test : " + date);
 
-            // 20190509
-            // 2019년 0509
-            stringBuffer.insert(4, "년 ");
-            // 2019년 05월 09
-            stringBuffer.insert(8, "월 ");
-            stringBuffer.insert(12, "일");
+            // 2019.0509
+            stringBuffer.insert(4, ".");
+            // 2019.05.09
+            stringBuffer.insert(7, ".");
 
             return stringBuffer.toString();
         }catch (Exception e) {
